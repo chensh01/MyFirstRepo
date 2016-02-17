4409d3831ac14bbb9232affcc9d6772ce472dd2e
chensh01 <chensh01@myemail.com>
2016-02-16 23:24:24 -0500

    older task done

--- Tutorial/src/FirstClass.java
+++ Tutorial/src/FirstClass.java
@@ -2,7 +2,7 @@
 public class FirstClass {
     public void dispgreeting(){
         System.out.println("Hello from inner class");
-        System.out.println("change half done");
+        System.out.println("change half done,all done");
     }
 
 }
