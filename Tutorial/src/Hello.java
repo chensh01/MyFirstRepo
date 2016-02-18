987dd8106898be60a55717fc4231ae2d10e75fba
chensh01 <chensh01@myemail.com>
2016-02-18 14:33:58 -0500

    other update

--- Tutorial/src/Hello.java
+++ Tutorial/src/Hello.java
@@ -1,14 +1,11 @@
-
 public class Hello {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Hello!");
-        System.out.println("Sencond Messgae");
         FirstClass Disp = new FirstClass();
         Disp.dispgreeting();
-        System.out.println("add first branch");
-        System.out.println("higher priority task");
+        System.out.println("First Branch");
 
     }
 
